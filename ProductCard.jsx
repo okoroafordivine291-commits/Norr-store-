25362926f51b94b7d024507085350e4099e67357
Okoroafor Divine <okoroafordivine291@gmail.com>
2026-07-17 23:52:03 +0100

    Fix import path for CartContext in ProductCard

--- ProductCard.jsx
+++ ProductCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { useCart } from '../context/CartContext.jsx'
+import { useCart } from '/CartContext.jsx'
 
 export default function ProductCard({ product }) {
   const { addItem } = useCart()
