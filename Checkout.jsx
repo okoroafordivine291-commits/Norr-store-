d3925357051aad0e8a41b4f754b056f1c37e7d5b
Okoroafor Divine <okoroafordivine291@gmail.com>
2026-07-17 23:47:50 +0100

    Fix import path for CartContext in Checkout.jsx

--- Checkout.jsx
+++ Checkout.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useCart } from '../context/CartContext.jsx'
+import { useCart } from '/CartContext.jsx'
 
 export default function Checkout() {
   const { items, subtotal, clearCart } = useCart()
