e6d5fe3e985b3c8dbd455f9ea891500be3049aca
Okoroafor Divine <okoroafordivine291@gmail.com>
2026-07-17 23:51:27 +0100

    Fix import path for CartContext in Navbar

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useCart } from '../context/CartContext.jsx'
+import { useCart } from '/CartContext.jsx'
 
 export default function Navbar() {
   const { count } = useCart()
